47caf34c0d43980c807de8d8fd9174f9a868288b
Angela Vuong <vuong.angela@gmail.com>
2019-12-03 21:04:39 -0500

    add find-largest-num file

---
+++ find-largest-num.js
@@ -0,0 +1,11 @@
+function findLargestNum(A){
+    let largestNumSoFar = A[0];
+    for(let i = 0; i < A.length; i++){
+        if(A[i] > largestNumSoFar){
+            largestNumSoFar = A[i];
+        }
+    }
+    return largestNumSoFar;
+}
+
+console.log(findLargestNum([9, 2, 3, 4, 5]));
